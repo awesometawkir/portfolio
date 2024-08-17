bb10cec0de0bb023baf19c533c836404cf4d6d6a
awesometawkir <ubiquitouss@yahoo.com>
2024-08-17 21:13:48 +0600

    next to it i wil start with rect route

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,39 @@
 import { useState, useEffect } from 'react';
 import Link from '../components/Link';
+import List from '../components/List';
 import './Profile.css';
 
 function Profile({ userName }) {
   const [loading, setLoading] = useState(false);
   const [profile, setProfile] = useState({});
+
+  const items = [
+    {
+      field: 'html_url',
+      value: (
+        <Link
+          url={profile.html_url}
+          title={profile.html_url}
+        />
+      ),
+    },
+    {
+      field: 'repos_url',
+      value: (
+        <Link
+          url={profile.repos_url}
+          title={profile.repos_url}
+        />
+      ),
+    },
+    { field: 'name', value: profile.name },
+    { field: 'company', value: profile.company },
+    { field: 'location', value: profile.location },
+    { field: 'email', value: profile.email },
+    { field: 'bio', value: profile.bio },
+  ];
+
+  
 
   useEffect(() => {
     async function fetchData() {
@@ -32,43 +61,7 @@ function Profile({ userName }) {
                 alt={profile.name}
            />
 
-        <ul>
-            <li>
-                <span>html_url: </span>
-                <Link
-                url={profile.html_url}
-                title={profile.html_url}
-                />
-            </li>
-            <li>
-                <span>repos_url: </span>
-                <Link
-                    url={profile.repos_url}
-                    title={profile.repos_url}
-                />
-            </li>
-
-            <li>
-                <span>name: </span>
-                {profile.name}
-            </li>
-            <li>
-                <span>company: </span>
-                {profile.company}
-            </li>
-            <li>
-                <span>location: </span>
-                {profile.location}
-            </li>
-            <li>
-                <span>email: </span>
-                {profile.email}
-            </li>
-                <li>
-                <span>bio: </span>
-            {profile.bio}
-            </li>
-        </ul>
+        <List items = {items} />
         </div>
       )}
     </div>
